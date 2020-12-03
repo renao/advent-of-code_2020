eb88e387c93cf4f7f1816c496c606fa359964c9d
René Wernicke <r.wernicke@prosoz.de>
2020-12-03 22:45:44 +0100

    Solve day1 puzzle first part

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -1,42 +1,27 @@
 defmodule Day1 do
-  @moduledoc """
-  Documentation for `Day1`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day1.hello()
-      :world
-
-  """
-  def hello do
-    find_answer()
-    :world
+  def find_answer_for_two_numbers do
+    load_entries()
+    |> find_solution
   end
 
-  def find_answer do
-    entries = load_entries()
-    load_entries
-    |> combines_to(2020)
-    # |> multiply_pair
+  def find_answer_for_three_numbers do
   end
 
   def load_entries do
     File.stream!("seed/entries")
     |> Enum.to_list
-    |> Enum.map(&parse_number/1);
+    |> Enum.map(&parse_number/1)
   end
 
-  def combines_to(entries, combined_value) do
-    # TODO
-    # Low Bob idea:
-    # Sum every entry combination and check for combined_value equality
-    # if a combination is found stop and return values.
+  def find_solution(entries) do
+    x = Enum.find(entries, fn x -> Enum.find(entries, fn y -> is_solution?(x, y) end) end)
+    y = 2020 - x
+    x * y
+  end
 
-    Enum.filter(entries, fn x -> Enum.filter(entries, fn y -> x + y == combined_value end) end)
+  def is_solution?(x, y) do
+    (x + y == 2020)
   end
 
   def parse_number(entry) do
--- day1/test/day1_test.exs
+++ day1/test/day1_test.exs
@@ -2,9 +2,11 @@ defmodule Day1Test do
   use ExUnit.Case
   doctest Day1
 
-  test "solves the puzzle" do
-    # Find the to numbers, that sum up to 2020 and multiply them.
-    surely_not_expected = 0
-    assert Day1.find_answer == surely_not_expected
+  test "solves the puzzle for two number" do
+    assert Day1.find_answer_for_two_numbers == 381699
+  end
+
+  test "solves the puzzle for three numbers" do
+    assert Day1.find_answer_for_three_numbers == 0
   end
 end
